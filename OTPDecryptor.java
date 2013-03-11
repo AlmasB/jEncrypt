d229adecc617b9261434d733a7e7b79efa5dee59
Seimour <toll.everon@gmail.com>
2013-03-11 19:14:49 +0000

    Update OTPDecryptor.java
    To fit changes in the super class

--- OTPDecryptor.java
+++ OTPDecryptor.java
@@ -1,14 +1,12 @@
 public class OTPDecryptor extends Translator
 {
-	public String process(String text, String key)
-	{
-		String innerKey = getInnerKey(text.length(), key);
-		
+	public String process(final String text, final String key)
+	{	
 		String decrypted = "";
 		
 		for (int i = 0; i < text.length(); i++)
 		{
-			decrypted += combine(text.charAt(i), innerKey.charAt(i), '-');
+			decrypted += combine(text.charAt(i), key.charAt(i), '-');
 		}
 		
 		return decrypted;
