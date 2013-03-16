2a846424926aea47743d9651158afe502b79e945
Seimour <toll.everon@gmail.com>
2013-03-16 15:17:46 +0000

    Update OTPDecryptor.java
    validation is now in 'process' method

--- OTPDecryptor.java
+++ OTPDecryptor.java
@@ -4,11 +4,16 @@ public class OTPDecryptor extends Translator
 	 * Decrypts text with the given key
 	 * @param text - text to decrypt
 	 * @param key - key to use in decryption
-	 * @return decrypted text
+	 * @return decrypted text or error message
 	 */
 	
 	public String process(final String text, final String key)
 	{	
+		String mes = validate(text, key);
+		
+		if (!mes.isEmpty())
+			return mes;
+
 		String decrypted = "";
 		
 		for (int i = 0; i < text.length(); i++)
