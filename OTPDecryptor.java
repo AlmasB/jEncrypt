cf7fa025e58a0a351a8e16c8fe8db705bde0954b
Seimour <toll.everon@gmail.com>
2013-03-16 13:28:50 +0000

    Update OTPDecryptor.java
    added comments

--- OTPDecryptor.java
+++ OTPDecryptor.java
@@ -1,5 +1,12 @@
 public class OTPDecryptor extends Translator
 {
+	/**
+	 * Decrypts text with the given key
+	 * @param text - text to decrypt
+	 * @param key - key to use in decryption
+	 * @return decrypted text
+	 */
+	
 	public String process(final String text, final String key)
 	{	
 		String decrypted = "";
