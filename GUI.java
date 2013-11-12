5fe527614afbc39e56a135799c0432d079bfc614
AlmasB <toll.everon@gmail.com>
2013-11-12 19:40:07 +0000

    yet another test

--- GUI.java
+++ GUI.java
@@ -6,6 +6,8 @@ public class GUI extends JFrame
 {
 	private static final int H = 970; // Height of window
 	private static final int W = 590; // Width of window
+    
+    //test
 	
 	private static final int LM1 = 10;	//left margin
 	private static final int LM2 = 300;	//Second Left margin (2nd column)
