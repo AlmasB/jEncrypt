c1c83c4947bb2c9efd1c0a8adb733f1ae3a5dac0
AlmasB <almaslvl@gmail.com>
2016-10-08 23:32:49 +0100

    maven dir model, cleanup

--- src/com/almasb/jencrypt/JEncryptMain.java
+++
@@ -1,9 +0,0 @@
-package com.almasb.jencrypt;
-
-import javafx.application.Application;
-
-public final class JEncryptMain {
-    public static void main(String[] args) {
-        Application.launch(App.class, args);
-    }
-}
--- src/main/java/com/almasb/jencrypt/JEncryptApp.java
+++ src/main/java/com/almasb/jencrypt/JEncryptApp.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App extends Application {
+public class JEncryptApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -14,5 +14,9 @@ public class App extends Application {
 
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
